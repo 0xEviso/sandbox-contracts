cf0a85a76b54beb2376b60b9ddeea71ca3eb6028
Eviso <eviso@protonmail.com>
2023-08-30 15:41:35 +0700

    update curve interface

--- src/interfaces/ICurveFi.sol
+++ src/interfaces/ICurveFi.sol
@@ -39,11 +39,11 @@ interface ICurveFi {
     ) external;
 
     function exchange(
-        int128 from,
-        int128 to,
+        uint256 from,
+        uint256 to,
         uint256 _from_amount,
         uint256 _min_to_amount
-    ) external payable;
+    ) external payable returns (uint256);
 
     function balances(int128) external view returns (uint256);
 
